6b5d8ec65e0f558694db203fdd6fc9f3165afdda
Amjith Ramanujam <amjith.r@gmail.com>
2015-02-09 21:07:58 -0800

    Fix the insert grammar and add tests.

--- tests/test_postgresql.py
+++ tests/test_postgresql.py
@@ -1,22 +1,30 @@
 import pytest
 
 queries = [
+    # SELECT
     # Keyword completion
     ('SELEC', ['SELECT']),
     ('sElEc', ['SELECT']),
     ('SELECT', []),
+    ('select * f', ['FROM']),
     ('select * fro', ['FROM']),
     ('select * from table a', ['AS']),
     ('select * from table order by attr as', ['ASC']),
     ('select column from table o', ['ORDER', 'OFFSET']),
-    ('in', ['INSERT']),
     # recursion
     ('select * from (s', ['SELECT']),
     ('select * from (select * from table) a', ['AS']),
     ('select * from (select * from t', ['table1', 'table2']),
     # variable matching
     ('select col', ['column']),
-    ('select * from tab', ['table1', 'table2'])
+    ('select * from tab', ['table1', 'table2']),
+
+    # INSERT
+    # Keyword completion
+    ('in', ['INSERT']),
+    # variable matching
+    ('insert into t', ['table1', 'table2']),
+    ('insert into table1 (c', ['column']),
 ]
 
 
